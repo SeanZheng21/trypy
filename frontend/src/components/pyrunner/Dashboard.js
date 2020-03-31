0307bdac20d98a8e5fcef65f4e85ae33df80210c
Carlistle Zheng <CarlistleZheng@gmail.com>
2020-03-31 10:43:57 +0200

    Implement React

---
+++ frontend/src/components/pyrunner/Dashboard.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react'
+import PyCode from "./PyCode";
+
+export default function Dashboard() {
+    return (
+        <Fragment>
+            <PyCode />
+        </Fragment>
+    )
+}
---
+++ frontend/src/components/pyrunner/PyCode.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class PyCode extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Write your Python code here!</h1>
+            </div>
+        );
+    }
+}
+
+export default PyCode
