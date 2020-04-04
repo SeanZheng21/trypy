f8a80f68b519d3be58f6a7c3ef2f39e6e292f1c3
Carlistle Zheng <CarlistleZheng@gmail.com>
2020-04-04 10:18:31 +0200

    Add code model

--- pyrunner/models.py
+++ pyrunner/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Code(models.Model):
+    name = models.CharField(max_length=64)
+    content = models.CharField(max_length=1024)
+    owner = models.IntegerField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
